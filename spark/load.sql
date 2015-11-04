f1f3efd5d5cb3dd527570ecedf668723c104ef7f
kyle hamilton <kyleih@optonline.net>
2015-11-04 11:49:16 -0500

    add spark

---
+++ spark/load.sql
@@ -0,0 +1,32 @@
+DROP TABLE tweets
+
+ADD JAR /data/w205Project/spark/hive-serdes-1.0-SNAPSHOT.jar;
+
+CREATE EXTERNAL TABLE tweets (
+  id BIGINT,
+  created_at STRING,
+  source STRING,
+  favorited BOOLEAN,
+  retweeted_status STRUCT<
+    text:STRING,
+    user:STRUCT<screen_name:STRING,name:STRING>,
+    retweet_count:INT>,
+  entities STRUCT<
+    urls:ARRAY<STRUCT<expanded_url:STRING>>,
+    user_mentions:ARRAY<STRUCT<screen_name:STRING,name:STRING>>,
+    hashtags:ARRAY<STRUCT<text:STRING>>>,
+  text STRING,
+  user STRUCT<
+    screen_name:STRING,
+    name:STRING,
+    friends_count:INT,
+    followers_count:INT,
+    statuses_count:INT,
+    verified:BOOLEAN,
+    utc_offset:INT,
+    time_zone:STRING>,
+  in_reply_to_screen_name STRING
+) 
+PARTITIONED BY (datehour INT)
+ROW FORMAT SERDE 'com.cloudera.hive.serde.JSONSerDe'
+LOCATION '/user/flume/tweets';
