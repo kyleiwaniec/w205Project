386f86c66c824e0041d8575209fdab4c0af25f17
kyle hamilton <kyleih@optonline.net>
2015-11-04 09:34:51 -0500

    modify install script

--- flume/install-flume.sh
+++ flume/install-flume.sh
@@ -3,5 +3,11 @@ mv /usr/lib/flume-ng/lib/twitter4j-core-3.0.3.jar /usr/lib/flume-ng/lib/twitter4
 mv /usr/lib/flume-ng/lib/twitter4j-stream-3.0.3.jar /usr/lib/flume-ng/lib/twitter4j-stream-3.0.3.jar.bak
 
 
-sudo -u hdfs hdfs dfs -mkdir /user/flume
-sudo -u hdfs hdfs dfs -mkdir /user/flume/tweets/
+echo -n "Are you running this for the first time, and thus don't have /user/flume/tweets/ hdfs dirs [y/n]: "
+read answer
+
+if [[ "$answer" == "y" ]]; then
+	sudo -u hdfs hdfs dfs -mkdir /user/flume
+	sudo -u hdfs hdfs dfs -mkdir /user/flume/tweets/
+	return 1
+fi
