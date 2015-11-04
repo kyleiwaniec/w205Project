eab03fb447d9df1e0894591c7003f2cb4c61c5ac
kyle hamilton <kyleih@optonline.net>
2015-11-04 09:09:49 -0500

    move the file back

--- install-flume.sh
+++
@@ -1,7 +0,0 @@
-yum install flume-ng
-mv /usr/lib/flume-ng/lib/twitter4j-core-3.0.3.jar /usr/lib/flume-ng/lib/twitter4j-core-3.0.3.jar.bak
-mv /usr/lib/flume-ng/lib/twitter4j-stream-3.0.3.jar /usr/lib/flume-ng/lib/twitter4j-stream-3.0.3.jar.bak
-
-
-sudo -u hdfs hdfs dfs -mkdir /user/flume
-sudo -u hdfs hdfs dfs -mkdir /user/flume/tweets/
