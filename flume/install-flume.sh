de6988f266c20b91858259acecae62778d3c5c72
root <root@ip-172-31-54-121.ec2.internal>
2015-11-04 14:36:21 +0000

    resolve conflict

--- flume/install-flume.sh
+++ flume/install-flume.sh
@@ -2,10 +2,6 @@ yum install flume-ng
 mv /usr/lib/flume-ng/lib/twitter4j-core-3.0.3.jar /usr/lib/flume-ng/lib/twitter4j-core-3.0.3.jar.bak
 mv /usr/lib/flume-ng/lib/twitter4j-stream-3.0.3.jar /usr/lib/flume-ng/lib/twitter4j-stream-3.0.3.jar.bak
 
-<<<<<<< HEAD
-sudo -u hdfs hdfs dfs -mkdir /user/flume/
-sudo -u hdfs hdfs dfs -mkdir /user/flume/tweets/
-=======
 
 echo -n "Are you running this for the first time, and thus don't have /user/flume/tweets/ hdfs dirs [y/n]: "
 read answer
@@ -15,4 +11,3 @@ if [[ "$answer" == "y" ]]; then
 	sudo -u hdfs hdfs dfs -mkdir /user/flume/tweets/
 	return 1
 fi
->>>>>>> 386f86c66c824e0041d8575209fdab4c0af25f17
