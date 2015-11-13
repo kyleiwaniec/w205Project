ca4f08d46a081d95624421137494514288ac9cfb
kyle hamilton <kyleih@optonline.net>
2015-11-13 14:51:25 -0500

    sparky-malarky

--- spark/getLinks.py
+++ spark/getLinks.py
@@ -6,7 +6,8 @@ from pyspark.sql import functions as F
 from pyspark.sql.window import Window
 
 sqlContext.sql("ADD JAR /data/w205Project/load/hive-serdes-1.0-SNAPSHOT.jar");
-
+sqlContext.sql("ADD JAR /usr/lib/hadoop/hadoop-aws.jar");
+#sqlContext.sql("ADD JAR /usr/lib/hadoop/lib/aws-java-sdk-1.7.14.jar");
 
 links = sqlContext.sql("select entities.urls.url[0] as tco, entities.urls.expanded_url[0] as link from tweets where entities.urls.url[0] IS NOT NULL");
 #links.show(10) 
@@ -15,7 +16,7 @@ links = sqlContext.sql("select entities.urls.url[0] as tco, entities.urls.expand
 uniqueLInks = links.dropDuplicates(['tco', 'link'])
 #links.na.drop()
 
-#uniqueLInks.toJSON()
-
-uniqueLInks.write.mode('append').json("s3n://w205twitterproject/links.json")
-#dataframe.repartition(1).save("s3n://mybucket/testfile","json")
+#uniqueLInks.rdd.saveAsTextFile("s3://w205twitterproject/attempt2/links2.json")
+#uniqueLInks.toJSON("s3://w205twitterproject/attempt2/links.json")
+#uniqueLInks.write.mode('append').json("s3://w205twitterproject/attempt2")
+uniqueLInks.repartition(1).save("s3n://w205twitterproject/links3","json")
