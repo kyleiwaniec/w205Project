8e180bcdb56af2cbe027b9107496caf40a973d9a
root <root@ip-172-31-12-183.ec2.internal>
2015-11-14 02:11:11 +0000

    scrapy spider now able to crawl defined 'start_urls' and write them to a txt file.

--- python/url_spider/url_spider/spiders/tweet_url_spider.py
+++ python/url_spider/url_spider/spiders/tweet_url_spider.py
@@ -1,5 +1,14 @@
 import scrapy
 
+
+def is_url(url):
+	return url.startswith('http') or url.startswith('file') or url.startswith('ssh') or url.startswith('ftp') or url.startswith('ftp') or url.startswith('ssl')
+
+def get_urls(response):
+	extracted_urls_response = response.xpath('//a/@href').extract()
+	extracted_urls = filter(lambda x: is_url(x), extracted_urls_response)
+	return extracted_urls
+
 class TweetURLSpider(scrapy.Spider):
 	name = "TweetURLSpider"
 	start_urls = [
@@ -9,9 +18,25 @@ class TweetURLSpider(scrapy.Spider):
 	allowed_domains = ["dmoz.org"]
 
 	def parse(self, response):
-		filename = response.url.split("/")[-2] + '.hmtl'
-		with open(filename, 'wb') as f:
-			f.write(response.body)
+	#	filename = response.url.split("/")[-2] + '.hmtl'
+	#	with open(filename, 'wb') as f:
+	#		f.write(response.body)
+		for href in get_urls(response):
+			url = response.urljoin(href)
+			filename = "spammy_urls.txt" 
+			with open(filename, 'ab') as f:
+				f.write(url + "\n")
+			print "URL scraped at first level: ", url
+			yield scrapy.Request(url, callback=self.parse_next_urls)
+
+	def parse_next_urls(self, response):
+		for href in get_urls(response):
+			url = response.urljoin(href)
+			filename = "spammy_urls.txt"
+                        with open(filename, 'ab') as f:
+                                f.write(url + "\n")
+			print "URL scraped at second level: ", url
+			yield scrapy.Request(url, self.parse_next_urls)
 # TODO:
 #	- Work on below code to retrieve urls
 # get_urls = filter(lambda x: x.startswith('http') or x.startswith('file') or x.startswith('ssh') or x.startswith('ftp') or x.startswith('ssl'), response.xpath('//a/@href').extract())
