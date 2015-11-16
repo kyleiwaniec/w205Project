50fd615851aada4a0f28717fdc87236676f7e455
root <root@ip-172-31-12-183.ec2.internal>
2015-11-16 00:11:17 +0000

    more scrapy-in around

--- python/url_spider/url_spider/spiders/tweet_url_spider.py
+++ python/url_spider/url_spider/spiders/tweet_url_spider.py
@@ -1,5 +1,10 @@
 import scrapy
+from snakebite.client import Client
+import os
 
+# TODO:
+# - Retrieve the urls that will start the crawler before initializing the crawler
+# - Push urls to S3 bucket
 
 def is_url(url):
 	return url.startswith('http') or url.startswith('file') or url.startswith('ssh') or url.startswith('ftp') or url.startswith('ftp') or url.startswith('ssl')
@@ -9,12 +14,18 @@ def get_urls(response):
 	extracted_urls = filter(lambda x: is_url(x), extracted_urls_response)
 	return extracted_urls
 
+# def hdfs_write(file):
+	# client = Client("localhost", 8020, use_trash=False)
+	
+
 class TweetURLSpider(scrapy.Spider):
 	name = "TweetURLSpider"
 	start_urls = [
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
+	# hdfs_output = "hello"
+	# print hdfs_output
 	allowed_domains = ["dmoz.org"]
 
 	def parse(self, response):
@@ -30,13 +41,16 @@ class TweetURLSpider(scrapy.Spider):
 			yield scrapy.Request(url, callback=self.parse_next_urls)
 
 	def parse_next_urls(self, response):
+		# hdfs_output = "hello"
 		for href in get_urls(response):
 			url = response.urljoin(href)
 			filename = "spammy_urls.txt"
                         with open(filename, 'ab') as f:
                                 f.write(url + "\n")
 			print "URL scraped at second level: ", url
+			# hdfs_output += hdfs_output + url + "\n"
 			yield scrapy.Request(url, self.parse_next_urls)
+		# os.system('echo "%s" | sudo -u hdfs hdfs dfs -put - /user/w205/test_tweets/test.txt' %(hdfs_output))
 # TODO:
 #	- Work on below code to retrieve urls
 # get_urls = filter(lambda x: x.startswith('http') or x.startswith('file') or x.startswith('ssh') or x.startswith('ftp') or x.startswith('ssl'), response.xpath('//a/@href').extract())
