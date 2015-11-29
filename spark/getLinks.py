4bb7e033b21802d536a8a576d22282b34c7c21ff
kyle hamilton <kyleih@optonline.net>
2015-11-29 10:16:10 -0500

    wip..

--- spark/getLinks.py
+++ spark/getLinks.py
@@ -9,17 +9,31 @@ sqlContext.sql("ADD JAR /data/w205Project/load/hive-serdes-1.0-SNAPSHOT.jar");
 sqlContext.sql("ADD JAR /usr/lib/hadoop/hadoop-aws.jar");
 #sqlContext.sql("ADD JAR /usr/lib/hadoop/lib/aws-java-sdk-1.7.14.jar");
 
+###############################################
+#    EXTRACT ALL THE LINKS INDISCRIMINATELY   #
+###############################################
+
+'''
 links = sqlContext.sql("select entities.urls.url[0] as tco, entities.urls.expanded_url[0] as link from tweets where entities.urls.url[0] IS NOT NULL");
-#links.show(10) 
-#links.count() #how many rows ~100K
-
 uniqueLInks = links.dropDuplicates(['tco', 'link'])
-#links.na.drop()
-
-#uniqueLInks.rdd.saveAsTextFile("s3://w205twitterproject/attempt2/links2.json")
-#uniqueLInks.toJSON("s3://w205twitterproject/attempt2/links.json")
-#uniqueLInks.write.mode('append').json("s3://w205twitterproject/attempt2")
 uniqueLInks.repartition(1).save("s3n://w205twitterproject/links3","json")
+'''
 
-# once the file is in S3, it has to be made public.
-# TODO: aws cli script to make public
+
+###############################################
+#                  ANALYZE                    #
+###############################################
+
+
+tweets = sqlContext.sql("""
+	select 
+		text as tweet, 
+		urls, user_mentions, 
+		hashtags, 
+		user.friends_count as num_friends, 
+		user.followers_count as num_followers,
+		user.verified
+	from tweets 
+	""");
+
+tweets.show(5)
