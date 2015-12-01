48f5a81ba0f4e2e6ff343b694ff4f8026f63eb5c
carlosrodriguezcastillo <cerodriguez@berkeley.edu>
2015-11-30 23:07:36 -0500

    Cleaned up python files and readME

--- python/url_spider/url_spider/init_urls.py
+++ python/url_spider/url_spider/init_urls.py
@@ -1,3 +1,12 @@
+
+#=============   File Info   ============#
+
+__version__ = "1.0"
+
+#DESCRIPTION: This script pulls urls to initiate crawler
+
+#========================================#
+
 import scrapy
 from snakebite.client import Client
 import os
@@ -8,25 +17,27 @@ import logging
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-f= open('logs/temp_urls.log','w')
-
 key = os.environ.get("S3_ACCESS_KEY")
 secret_key = os.environ.get("S3_SECRET_ACCESS_KEY")
 endpoint = "s3-us-west-2.amazonaws.com"
 bucket = "w205twitterproject"
+init_store = "links2.json"
+local_store = "logs/temp_urls.log"
+
+f= open(local_store,'w')
 
 conn = util.s3_connect(key, secret_key, endpoint,default_bucket=bucket)
-response = util.get_json(conn,"links2.json")
+response = util.get_json(conn, init_store)
 
 links = util.get_links(response)
 
 for link in links:
 	try:
-		# link = unicode(str(link['link']).decode('utf-8'), errors='ignore')
-		f.write(str(link['link'])+'\n')
-		# logging.debug("Encoded")
+		tmp_link = str(link['link'])
+		f.write(tmp_link+'\n')
+		logging.debug("Writing to store %s , link %s" % (local_store, tmp_link))
 	except:
-		pass #logging.debug("Couldn't encode")
+		pass
 f.close()
 
 
--- python/url_spider/url_spider/url_upload.py
+++ python/url_spider/url_spider/url_upload.py
@@ -1,3 +1,11 @@
+#=============   File Info   ============#
+
+__version__ = "1.0"
+
+#DESCRIPTION: This script uploads scraped urls to S3 for plugin solution
+
+#========================================#
+
 import scrapy
 from snakebite.client import Client
 import os
@@ -8,27 +16,28 @@ import logging
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-f= open('logs/spammy_urls.log','rb')
+key = os.environ.get("S3_ACCESS_KEY")
+secret_key = os.environ.get("S3_SECRET_ACCESS_KEY")
+endpoint = "s3-us-west-2.amazonaws.com"
+bucket = "w205twitterproject"
+spam_url_location = "logs/spammy_urls.log"
+S3_url_location = "links2.json"
+
+f= open(spam_url_location,'rb')
 
 links = []
 for line in f:
 	json_obj = {"link":line.rstrip("\n")}
 	links.append(json_obj)
 
-key = os.environ.get("S3_ACCESS_KEY")
-secret_key = os.environ.get("S3_SECRET_ACCESS_KEY")
-endpoint = "s3-us-west-2.amazonaws.com"
-bucket = "w205twitterproject"
-
 conn = util.s3_connect(key, secret_key, endpoint,default_bucket=bucket)
-response = util.get_json(conn,"links2.json")
+response = util.get_json(conn,S3_url_location)
 
 logging.debug("original length of spammy urls list: %3f" % len(util.get_links(response)))
 
 util.append_links(response,links)
+
 logging.debug("after appending length of spammy urls list: %3f" % len(util.get_links(response)))
-logging.debug(type(response))
-logging.debug(type(json.dumps(response)))
 
 upload = json.dumps(response)
 
@@ -37,7 +46,3 @@ with open('logs/test_links.json', 'w') as outfile:
 
 f = open('test_links.json','r')
 util.upload_json(conn,"logs/test.json",f)
-
-#new_response = util.get_json(conn,"test.json")
-
-#logging.debug("original length of spammy urls list: %3f" % len(util.get_links(new_response)))
