a1feb5b79725c67fdccdf16c34c042879c1db888
kyle hamilton <kyleih@optonline.net>
2015-12-02 13:35:03 -0500

    cleanup load and transform scripts

--- load/add-partition.sh
+++
@@ -1,5 +0,0 @@
-#! bin/bash
-
-today=$(date +"%Y/%m/%d")
-location="/user/flume/tweets/$today"
-hive -e "ADD JAR /data/w205Project/load/hive-serdes-1.0-SNAPSHOT.jar; ALTER TABLE tweets ADD IF NOT EXISTS PARTITION (today_date = '$today') location '$location'; select * from tweets limit 1;"
