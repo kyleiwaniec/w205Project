19806ee15c66fa937f6aa98790b7f1c36ece29ec
Sharmila Velamur <sharmila.velamur@berkeley.edu>
2015-12-02 09:54:02 -0700

    Renaming Scala App-1

--- spark/user_classification/start-user-classification.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-/data/spark15_hadoop24/bin/spark-submit \
---class "mids.w205.spark.TransformTweetsData" \
---master local[2] \
-/data/w205Project/spark/user_classification/target/scala-2.11/user_classification_2.11-1.0.jar
