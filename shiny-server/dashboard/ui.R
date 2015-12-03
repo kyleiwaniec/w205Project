ba7f4919e755670dc8442c4f71b7f65276fbc61e
kyle hamilton <kyleih@optonline.net>
2015-12-03 13:37:15 -0500

    pretty ui

--- shiny-server/dashboard/ui.R
+++ shiny-server/dashboard/ui.R
@@ -3,29 +3,28 @@ library(ggplot2)
 
 fluidPage(
   includeCSS("www/styles.css"),
-  
-  tabsetPanel(
-    tabPanel('Honeypot data', tags$div(class="divclass",tags$h4("Following to Followers Ratio"), plotOutput('plot'))),
-    tabPanel('Following/Followers', plotOutput("postgresData")),
-    tabPanel('Word counts', 
-#      tags$h4(textOutput('texts')),
-      tags$div(class="col-sm-6",plotOutput('words_poll')),
-      tags$div(class="col-sm-6",plotOutput('words_leg'))
-      #tags$div(class="col-sm-6",textOutput('summary_poll')),
-      #tags$div(class="col-sm-6",verbatimTextOutput('summary_leg'))
-   ),
-   tabPanel('My hat', plotOutput("aliens"))
+
+
+    navlistPanel(
+      well = FALSE,
+      widths = c(3, 9),
+      tabPanel('Project home',tags$div(class="home", img(src = "img/home-bg.jpg"))), 
+
+      tabPanel('Honeypot', tags$div(class="divclass",tags$h4("Following to Followers Ratio"), plotOutput('plot'))),
+      tabPanel('Following/Followers', plotOutput("postgresData")),
+      tabPanel('Word counts', 
+  #      tags$h4(textOutput('texts')),
+        tags$div(class="col-sm-6",plotOutput('words_poll')),
+        tags$div(class="col-sm-6",plotOutput('words_leg'))
+        #tags$div(class="col-sm-6",textOutput('summary_poll')),
+        #tags$div(class="col-sm-6",verbatimTextOutput('summary_leg'))
+     ),
+      tabPanel('Recent Activity', ""),
+      tabPanel('Following/followers', ""),
+      tabPanel('Wordcount Frequency', "")
+      #tabPanel('My hat', plotOutput("aliens"))
+
+    )
 
 
 )
-
- 
-  #tags$div(class="divclass",printOutput('texts')),
-  
-  
-  
-  #mainPanel(
-    #plotOutput('plot')
-  #)
-)
-
