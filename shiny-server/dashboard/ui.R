89642f29756bec1ad708314711280c035fc5d8bb
root <root@ip-172-31-53-161.ec2.internal>
2015-12-03 04:35:52 +0000

    dashboard wip

--- shiny-server/dashboard/ui.R
+++ shiny-server/dashboard/ui.R
@@ -3,18 +3,18 @@ library(ggplot2)
 
 fluidPage(
   includeCSS("www/styles.css"),
- 
+  
   tabsetPanel(
     tabPanel('Honeypot data', tags$div(class="divclass",tags$h4("Following to Followers Ratio"), plotOutput('plot'))),
     tabPanel('Following/Followers', plotOutput("postgresData")),
     tabPanel('Word counts', 
-      tags$h4(textOutput('texts')),
+#      tags$h4(textOutput('texts')),
       tags$div(class="col-sm-6",plotOutput('words_poll')),
       tags$div(class="col-sm-6",plotOutput('words_leg'))
       #tags$div(class="col-sm-6",textOutput('summary_poll')),
       #tags$div(class="col-sm-6",verbatimTextOutput('summary_leg'))
    ),
-   tabPanel('Aliens', plotOutput("aliens"))
+   tabPanel('My hat', plotOutput("aliens"))
 
 
 )
