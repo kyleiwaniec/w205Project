4e82d228aede2fbb1b3b198f4ecaed37ddd9166c
kyle hamilton <kyleih@optonline.net>
2015-12-04 17:25:39 -0500

    twitter keys

---
+++ provision/twitter-keys.sh
@@ -0,0 +1,65 @@
+#! bin/bash
+
+#####################################
+# TWITTER KEYS
+#####################################
+
+# add twitter keys to environment:
+
+function consumerKey {
+	STR=$'Please enter your consumerKey: '
+	echo "$STR"
+	read answer
+
+	if [[ "$answer" != "" ]]; then
+		cat "TwitterAgent.sources.Twitter.consumerKey = $answer" > /data/w205Project/flume/conf/flume.conf 
+	else
+		STR=$'consumerKey can\'t be empty: \n'
+		echo "$STR"
+		consumerKey	
+	fi
+}
+function consumerSecret {
+	STR=$'Please enter your consumerSecret: '
+	echo "$STR"
+	read answer
+
+	if [[ "$answer" != "" ]]; then
+		cat "TwitterAgent.sources.Twitter.consumerSecret = $answer" > /data/w205Project/flume/conf/flume.conf 
+	else
+		STR=$'consumerSecret can\'t be empty: \n'
+		echo "$STR"
+		consumerSecret	
+	fi
+}
+function accessToken {
+	STR=$'Please enter your accessToken: '
+	echo "$STR"
+	read answer
+
+	if [[ "$answer" != "" ]]; then
+		cat "TwitterAgent.sources.Twitter.accessToken = $answer" > /data/w205Project/flume/conf/flume.conf 
+	else
+		STR=$'accessToken can\'t be empty: \n'
+		echo "$STR"
+		accessToken	
+	fi
+}
+function accessTokenSecret {
+	STR=$'Please enter your accessTokenSecret: '
+	echo "$STR"
+	read answer
+
+	if [[ "$answer" != "" ]]; then
+		cat "TwitterAgent.sources.Twitter.accessTokenSecret = $answer" > /data/w205Project/flume/conf/flume.conf 
+	else
+		STR=$'accessTokenSecret can\'t be empty: \n'
+		echo "$STR"
+		accessTokenSecret	
+	fi
+}
+
+consumerKey
+consumerSecret
+accessToken
+accessTokenSecret
