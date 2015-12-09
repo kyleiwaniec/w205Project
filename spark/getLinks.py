a520f2dbb3714d1f234d7093312e642dc9fc9153
kyle hamilton <kyleih@optonline.net>
2015-12-09 17:33:12 -0500

    ...

--- spark/getLinks.py
+++ spark/getLinks.py
@@ -190,6 +190,7 @@ from sqlalchemy import create_engine
 engine = create_engine('postgresql://postgres:pass@localhost:5432/twitter')
 con = engine.connect()
 print pdf.shape
+print pdf.columns
 pdf.to_sql(con=con, name='twitters', if_exists='append', flavor='postgresql')
 
 '''
