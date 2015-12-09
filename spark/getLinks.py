fd119807134bedbb4b5b349fe1f4da796652bd9e
kyle hamilton <kyleih@optonline.net>
2015-12-09 14:38:14 -0500

    reactive

--- spark/getLinks.py
+++ spark/getLinks.py
@@ -122,7 +122,9 @@ print "fitting the model..."
 
 train_cols = allData.columns[[6,7,8,12,13,14,15]]
 print train_cols
-
+# Index([u'num_following', u'num_followers', u'num_tweets', u'num_words',
+#        u'num_hashtags', u'num_urls', u'num_mentions'],
+#       dtype='object')
 
 logit = sm.Logit(allData['isPolluter'], allData[train_cols])
 model = logit.fit()
@@ -142,6 +144,9 @@ pdf = newdata.toPandas()
 
 predict_cols = pdf.columns[[9,10,11,3,16,14,15]]
 print predict_cols
+# Index([u'num_following', u'num_followers', u'num_tweets', u'num_words',
+#        u'num_hastags', u'num_urls', u'num_mentions'],
+#       dtype='object')
 
 pdf['isPolluter'] = model.predict(pdf[predict_cols])
 
