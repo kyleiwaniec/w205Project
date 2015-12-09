361457997e1733473f54198904765bbb5b8ec5ac
kyle hamilton <kyleih@optonline.net>
2015-12-09 18:16:47 -0500

    ...

--- spark/getLinks.py
+++ spark/getLinks.py
@@ -190,7 +190,7 @@ engine = create_engine('postgresql://postgres:pass@localhost:5432/twitter')
 con = engine.connect()
 print pdf.shape
 print pdf.columns
-pdf.to_sql(con=con, name='twitters', if_exists='append', flavor='postgresql')
+pdf.to_sql(con=con, name='twitters', if_exists='replace', flavor='postgresql')
 
 '''
 if_exists: {'fail', 'replace', 'append'}, default 'fail'
