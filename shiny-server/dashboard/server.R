6dbdeb7e2afe5feab2dfd66f3081f86e69654ee8
kyle hamilton <kyleih@optonline.net>
2015-12-13 09:44:13 -0500

    add loading indicators

--- shiny-server/dashboard/server.R
+++ shiny-server/dashboard/server.R
@@ -1,4 +1,4 @@
-
+#hello
 library(shiny)
 library(ggplot2)
 library(RPostgreSQL)
@@ -21,76 +21,6 @@ load_data <- function(){
     ( SELECT DISTINCT 1 + trunc(random() * (select max(id) from twitters))::integer AS id  
     FROM generate_series(1, 100000) g) r JOIN  twitters USING (id) LIMIT  100000;")
 
-
-
-# SELECT *, AVG(num_tweets) AS avg_num_tweets FROM twitters GROUP BY 
-#       user_id,id,index,row_created,tweet_id,tweet,num_words,created_ts,user_created_ts,tweet_created_ts,
-#       screen_name,name,num_following,num_followers,num_tweets,retweeted,retweet_count,num_urls,        
-#       num_mentions,num_hashtags,user_profile_url,tweeted_urls,is_polluter
-
-# SELECT * FROM 
-#     ( SELECT DISTINCT 1 + trunc(random() * (select max(id) from twitters))::integer AS id  
-#     FROM generate_series(1, 100000) g) r JOIN  twitters USING (id) LIMIT  100000;
-
-
-# SELECT * FROM twitters
-#         WHERE index IN (
-#                SELECT round(random() * 21e6)::integer as index
-#                FROM generate_series(1, 110000)
-#                GROUP BY index 
-#                )
-#         LIMIT 100000
-
-
-# ^^ GROUP BY index --> Discard duplicates
-
-# some fancy wierdness
-# SELECT * FROM  
-#       (SELECT DISTINCT 1 + trunc(random() * (
-#               SELECT reltuples::bigint AS estimate
-#               FROM   pg_class
-#               WHERE  oid = 'public.twitters'::regclass
-#             )
-#         )::integer AS index 
-#         FROM generate_series(1, 11000) g) r 
-#         JOIN  twitters USING (index) LIMIT  10000;
-
-# count(*) the number of rows: meh...
-
-# WITH params AS (
-#     SELECT count(*) AS ct             
-#      , min(index)  AS min_id
-#      , max(index)  AS max_id
-#      , max(index) - min(index) AS id_span
-#     FROM   twitters
-#     )
-#     SELECT *
-#     FROM  (
-#         SELECT p.min_id + trunc(random() * p.id_span)::integer AS index
-#         FROM   params p
-#               ,generate_series(1, 110000) g 
-#         GROUP  BY 1                       
-#         ) r
-#     JOIN   twitters USING (index)
-#     LIMIT  100000;
-
-
-
-# approximate the number of rows manually: meh...
-
-# SELECT * FROM  
-#     ( SELECT DISTINCT 1 + trunc(random() * max(id))::integer AS id 
-#       FROM generate_series(1, 251000) g) r 
-#       JOIN  twitters USING (id) LIMIT  250000;
-
-
-# SELECT * FROM twitters TABLESAMPLE BERNOULLI (10); --Using BERNOULLI sampling method fails
-
-# SELECT * FROM twitters ORDER BY RANDOM() LIMIT 10000 --slow....
-
-
-
-
   dbDisconnect(con)
   #data = na.omit(data)
   data$is_polluter = ifelse(data$is_polluter > 0.85, 1,  0)
@@ -102,61 +32,33 @@ function(input, output) {
   
   twitters <- load_data()
 
-
-
-  # twitters
-  # [1] "index"            "user_id"          "tweet_id"         "tweet"           
-  # [5] "num_words"        "created_ts"       "user_created_ts"  "tweet_created_ts"
-  # [9] "screen_name"      "name"             "num_following"    "num_followers"   
-  # [13] "num_tweets"      "retweeted"        "retweet_count"    "num_urls"        
-  # [17] "num_mentions"    "num_hashtags"      "user_profile_url" "tweeted_urls"    
-  # [21] "is_polluter" 
+  # names(twitters)
+    # id 
+    # index 
+    # row_created 
+    # user_id 
+    # tweet_id 
+    # tweet 
+    # num_words 
+    # created_ts 
+    # user_created_ts 
+    # tweet_created_ts 
+    # screen_name 
+    # name 
+    # num_following 
+    # num_followers 
+    # num_tweets 
+    # retweeted 
+    # retweet_count 
+    # num_urls 
+    # num_mentions 
+    # num_hashtags 
+    # user_profile_url 
+    # tweeted_urls 
+    # is_polluter
     
   
-  #############################################################################################
-  # HONEYPOT
-  #############################################################################################
 
-  legitimate_users = read.delim("legitimate_users.txt", header=FALSE)
-  legitimate_users = na.omit(legitimate_users)
-  
-  content_polluters = read.delim("content_polluters.txt", header=FALSE)
-  content_polluters = na.omit(content_polluters)
-  
-  legitimate_users$isLegit = rep(1,nrow(legitimate_users))
-  content_polluters$isLegit = rep(0,nrow(content_polluters))
-
-  colNames = c("UserID","CreatedAt","CollectedAt","NumerOfFollowings",
-                "NumberOfFollowers","NumberOfTweets","LengthOfScreenName",
-                "LengthOfDescriptionInUserProfile", "isLegit")
-
-  names(legitimate_users) = colNames
-  names(content_polluters) = colNames
-
-  all_users = rbind(legitimate_users,content_polluters)
-  
-  fit_polluters = lm(NumberOfFollowers~NumerOfFollowings, data = content_polluters)
-  fit_legit = lm(NumberOfFollowers~NumerOfFollowings, data = legitimate_users)
-
-  output$plot <- renderPlot({
-      ggplot() +
-
-      geom_point(data = content_polluters, aes(log(NumberOfFollowers), log(NumerOfFollowings)), colour = "gold2", shape=1) +
-      geom_abline(slope=as.numeric(fit_polluters$coefficients[2]), colour='gold2') + 
-      geom_point(data = legitimate_users, aes(log(NumberOfFollowers), log(NumerOfFollowings)), colour = "darkolivegreen3", shape=1) +
-      geom_abline(slope=as.numeric(fit_legit$coefficients[2]), colour='darkolivegreen3') +      
-      #scale_x_continuous(limits = c(0, 200000)) +
-      theme(
-       # axis.text = element_text(size = 14),
-       # legend.key = element_rect(fill = "navy"),
-       # legend.background = element_rect(fill = "white"),
-       # legend.position = c(0.14, 0.80),
-       # panel.grid.major = element_line(colour = "grey40"),
-       # panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill = "white")
-      )    
-    
-  }, height=700)
 
   #############################################################################################
   # SHINY-TWEETS(TM)
@@ -173,19 +75,20 @@ function(input, output) {
 
 
   output$postgresData <- renderPlot({
+
+    # Create a Progress object
+    progress <- shiny::Progress$new()
+    # Make sure it closes when we exit this reactive, even if there's an error
+    on.exit(progress$close())
+    progress$set(message = "Making plot", value = 0)
+
+
     ggplot() +
       geom_point(data = polluters_ps, aes(log(num_followers), log(num_following)), colour = "gold2", shape=1) +
       geom_abline(slope=as.numeric(fitPolluters$coefficients[2]), colour='gold2') + 
       geom_point(data = legit_ps, aes(log(num_followers), log(num_following)), colour="darkolivegreen3",shape=1) +
       geom_abline(slope=as.numeric(fitLegit$coefficients[2]), colour="darkolivegreen3") +
-      #scale_x_continuous(limits = c(0, 200000)) +
       theme(
-       # axis.text = element_text(size = 14),
-       # legend.key = element_rect(fill = "navy"),
-       # legend.background = element_rect(fill = "white"),
-       # legend.position = c(0.14, 0.80),
-       # panel.grid.major = element_line(colour = "grey40"),
-       # panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white")
       )    
     
@@ -217,6 +120,8 @@ function(input, output) {
   output$N_poll2 <- renderPrint({
     cat("N = ", nrow(polluters_ps)[1])
   })
+
+
   ##############
   # num_words
   ##############
@@ -224,6 +129,13 @@ function(input, output) {
   max_words = max(max(polluters_ps$num_words), max(legit_ps$num_words))
   
   output$words_poll <- renderPlot({
+
+    # Create a Progress object
+    progress <- shiny::Progress$new()
+    # Make sure it closes when we exit this reactive, even if there's an error
+    on.exit(progress$close())
+    progress$set(message = "Making words plot", value = 0)
+
      hist(polluters_ps$num_words, col="gold2", border="white",main = paste("Content Polluters"), breaks=30, xlim=c(0,max_words))
          axis(1,col="gray100")
          axis(2,col="gray100")
@@ -261,10 +173,18 @@ function(input, output) {
   max_tweets = max(summary(polluters_ps$num_tweets)[5], summary(legit_ps$num_tweets)[5])
 
   output$tweets_poll <- renderPlot({
-     hist(polluters_ps$num_tweets, col="gold2", border="white",main = paste("Content Polluters"), breaks=600, xlim=c(0,max_tweets))
-     axis(1,col="gray100")
-     axis(2,col="gray100")
+
+    # Create a Progress object
+    progress <- shiny::Progress$new()
+    # Make sure it closes when we exit this reactive, even if there's an error
+    on.exit(progress$close())
+    progress$set(message = "Making plot", value = 0)  
+
+    hist(polluters_ps$num_tweets, col="gold2", border="white",main = paste("Content Polluters"), breaks=600, xlim=c(0,max_tweets))
+    axis(1,col="gray100")
+    axis(2,col="gray100")
   })
+  
   output$tweets_leg <- renderPlot({
     hist(legit_ps$num_tweets, col="darkolivegreen3", border="white", main=paste("Legitimate Users"),breaks=600, xlim=c(0,max_tweets))
     axis(1,col="gray100")
@@ -341,4 +261,131 @@ function(input, output) {
 
 
 
+  #############################################################################################
+  # HONEYPOT
+  #############################################################################################
+
+  legitimate_users = read.delim("legitimate_users.txt", header=FALSE)
+  legitimate_users = na.omit(legitimate_users)
+  
+  content_polluters = read.delim("content_polluters.txt", header=FALSE)
+  content_polluters = na.omit(content_polluters)
+  
+  legitimate_users$isLegit = rep(1,nrow(legitimate_users))
+  content_polluters$isLegit = rep(0,nrow(content_polluters))
+
+  colNames = c("UserID","CreatedAt","CollectedAt","NumerOfFollowings",
+                "NumberOfFollowers","NumberOfTweets","LengthOfScreenName",
+                "LengthOfDescriptionInUserProfile", "isLegit")
+
+  names(legitimate_users) = colNames
+  names(content_polluters) = colNames
+
+  all_users = rbind(legitimate_users,content_polluters)
+  
+  fit_polluters = lm(NumberOfFollowers~NumerOfFollowings, data = content_polluters)
+  fit_legit = lm(NumberOfFollowers~NumerOfFollowings, data = legitimate_users)
+
+  output$plot <- renderPlot({
+
+      # Create a Progress object
+      progress <- shiny::Progress$new()
+      # Make sure it closes when we exit this reactive, even if there's an error
+      on.exit(progress$close())
+      progress$set(message = "Making plot", value = 0)
+
+
+      ggplot() +
+
+      geom_point(data = content_polluters, aes(log(NumberOfFollowers), log(NumerOfFollowings)), colour = "gold2", shape=1) +
+      geom_abline(slope=as.numeric(fit_polluters$coefficients[2]), colour='gold2') + 
+      geom_point(data = legitimate_users, aes(log(NumberOfFollowers), log(NumerOfFollowings)), colour = "darkolivegreen3", shape=1) +
+      geom_abline(slope=as.numeric(fit_legit$coefficients[2]), colour='darkolivegreen3') +      
+      #scale_x_continuous(limits = c(0, 200000)) +
+      theme(
+       # axis.text = element_text(size = 14),
+       # legend.key = element_rect(fill = "navy"),
+       # legend.background = element_rect(fill = "white"),
+       # legend.position = c(0.14, 0.80),
+       # panel.grid.major = element_line(colour = "grey40"),
+       # panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = "white")
+      )    
+    
+  }, height=700)
+
+
 }
+
+
+################################################################
+# VARIOUS RANDOM SAMPLE QUERIES
+################################################################
+
+
+# SELECT *, AVG(num_tweets) AS avg_num_tweets FROM twitters GROUP BY 
+#       user_id,id,index,row_created,tweet_id,tweet,num_words,created_ts,user_created_ts,tweet_created_ts,
+#       screen_name,name,num_following,num_followers,num_tweets,retweeted,retweet_count,num_urls,        
+#       num_mentions,num_hashtags,user_profile_url,tweeted_urls,is_polluter
+
+# SELECT * FROM 
+#     ( SELECT DISTINCT 1 + trunc(random() * (select max(id) from twitters))::integer AS id  
+#     FROM generate_series(1, 100000) g) r JOIN  twitters USING (id) LIMIT  100000;
+
+
+# SELECT * FROM twitters
+#         WHERE index IN (
+#                SELECT round(random() * 21e6)::integer as index
+#                FROM generate_series(1, 110000)
+#                GROUP BY index 
+#                )
+#         LIMIT 100000
+
+
+# ^^ GROUP BY index --> Discard duplicates
+
+# some fancy wierdness
+# SELECT * FROM  
+#       (SELECT DISTINCT 1 + trunc(random() * (
+#               SELECT reltuples::bigint AS estimate
+#               FROM   pg_class
+#               WHERE  oid = 'public.twitters'::regclass
+#             )
+#         )::integer AS index 
+#         FROM generate_series(1, 11000) g) r 
+#         JOIN  twitters USING (index) LIMIT  10000;
+
+# count(*) the number of rows: meh...
+
+# WITH params AS (
+#     SELECT count(*) AS ct             
+#      , min(index)  AS min_id
+#      , max(index)  AS max_id
+#      , max(index) - min(index) AS id_span
+#     FROM   twitters
+#     )
+#     SELECT *
+#     FROM  (
+#         SELECT p.min_id + trunc(random() * p.id_span)::integer AS index
+#         FROM   params p
+#               ,generate_series(1, 110000) g 
+#         GROUP  BY 1                       
+#         ) r
+#     JOIN   twitters USING (index)
+#     LIMIT  100000;
+
+
+
+# approximate the number of rows manually: meh...
+
+# SELECT * FROM  
+#     ( SELECT DISTINCT 1 + trunc(random() * max(id))::integer AS id 
+#       FROM generate_series(1, 251000) g) r 
+#       JOIN  twitters USING (id) LIMIT  250000;
+
+
+# SELECT * FROM twitters TABLESAMPLE BERNOULLI (10); --Using BERNOULLI sampling method fails
+
+# SELECT * FROM twitters ORDER BY RANDOM() LIMIT 10000 --slow....
+
+
