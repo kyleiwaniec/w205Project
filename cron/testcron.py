f8c9d7a1a5fda5f82a059776a483ad6035ae593a
kyle hamilton <kyleih@optonline.net>
2015-12-17 19:52:36 -0500

    remove old files

--- cron/testcron.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-
-print  'Yo bro! Was uuuuuuup!'
--- cron/testcron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir /Users/koza/Documents/UCBerkeley/205/project/w205Project/cron/testdir
-echo "yo dawg - I made yer bleedin dir"
